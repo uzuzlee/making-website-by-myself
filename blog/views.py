f85b015a1b0a8b726e9030efd3c071e1b6cf7f2d
yuji <leech030916@gmail.com>
2025-11-05 19:35:57 +0900

    test

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def index(request):
+    posts= Post.objects.all().order_by('-created_at')
     return render(
         request,
         'blog/index.html',
-        
+        {
+            'posts':posts
+        }
+
     )
